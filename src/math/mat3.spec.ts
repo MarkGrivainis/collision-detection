ad09b66699145fadd26ecb3a47e6e4ecebad2e53
Mark Grivainis <markgrivainis@gmail.com>
2023-01-23 17:38:02 -0600

    feat: adds zero multiply test
    + adds test for zero matrix multiplication
    + fixes syntax errors

--- src/math/mat3.spec.ts
+++ src/math/mat3.spec.ts
@@ -1,9 +1,13 @@
-import { Mat3 } from './mat3';
-
+import { Mat3 } from "./mat3";
 
 describe("matrix multiplication", () => {
-    test("zero matrix", () => {
-        // const matA = new Mat3();
-        expect(true).toBe(false);      
-    })
-})
+  test("zero matrix", () => {
+    const matA = new Mat3();
+
+    const matB = new Mat3();
+    matB.set(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+    matA.multiply(matB);
+    expect(matA.values).toEqual(matB.values);
+  });
+});
--- src/math/mat3.ts
+++ src/math/mat3.ts
@@ -1,7 +1,28 @@
 export class Mat3 {
+  set(
+    a11: number,
+    a12: number,
+    a13: number,
+    a21: number,
+    a22: number,
+    a23: number,
+    a31: number,
+    a32: number,
+    a33: number
+  ) {
+    this.values[0] = a11;
+    this.values[1] = a12;
+    this.values[2] = a13;
+    this.values[3] = a21;
+    this.values[4] = a22;
+    this.values[5] = a23;
+    this.values[6] = a31;
+    this.values[7] = a32;
+    this.values[8] = a33;
+  }
   values: Float32Array;
   constructor() {
-    let temp = new Float32Array([1, 0, 0, 0, 1, 0, 0, 0, 1]);
+    const temp = new Float32Array([1, 0, 0, 0, 1, 0, 0, 0, 1]);
     this.values = temp;
   }
 
@@ -9,30 +30,30 @@ export class Mat3 {
     if (matrixB.values.length !== 9) {
       return;
     }
-    this.values[0] += matrixB[0];
-    this.values[1] += matrixB[1];
-    this.values[2] += matrixB[2];
-    this.values[3] += matrixB[3];
-    this.values[4] += matrixB[4];
-    this.values[5] += matrixB[5];
-    this.values[6] += matrixB[6];
-    this.values[7] += matrixB[7];
-    this.values[8] += matrixB[8];
+    this.values[0] += matrixB.values[0];
+    this.values[1] += matrixB.values[1];
+    this.values[2] += matrixB.values[2];
+    this.values[3] += matrixB.values[3];
+    this.values[4] += matrixB.values[4];
+    this.values[5] += matrixB.values[5];
+    this.values[6] += matrixB.values[6];
+    this.values[7] += matrixB.values[7];
+    this.values[8] += matrixB.values[8];
   }
 
   sub(matrixB: Mat3): void {
     if (matrixB.values.length !== 9) {
       return;
     }
-    this.values[0] -= matrixB[0];
-    this.values[1] -= matrixB[1];
-    this.values[2] -= matrixB[2];
-    this.values[3] -= matrixB[3];
-    this.values[4] -= matrixB[4];
-    this.values[5] -= matrixB[5];
-    this.values[6] -= matrixB[6];
-    this.values[7] -= matrixB[7];
-    this.values[8] -= matrixB[8];
+    this.values[0] -= matrixB.values[0];
+    this.values[1] -= matrixB.values[1];
+    this.values[2] -= matrixB.values[2];
+    this.values[3] -= matrixB.values[3];
+    this.values[4] -= matrixB.values[4];
+    this.values[5] -= matrixB.values[5];
+    this.values[6] -= matrixB.values[6];
+    this.values[7] -= matrixB.values[7];
+    this.values[8] -= matrixB.values[8];
   }
 
   scalerMultiply(scaler: number): void {
@@ -49,16 +70,27 @@ export class Mat3 {
   multiply(matrix: Mat3): void {
     // c_ij = sum k=1 -> n a_ik b_kj
 
-    const a11=this.values[0],a12=this.values[1], a13=this.values[2];
-    const a21=this.values[3],a22=this.values[4], a23=this.values[5];
-    const a31=this.values[6],a32=this.values[7], a33=this.values[8];
+    const a11 = this.values[0],
+      a12 = this.values[1],
+      a13 = this.values[2];
+    const a21 = this.values[3],
+      a22 = this.values[4],
+      a23 = this.values[5];
+    const a31 = this.values[6],
+      a32 = this.values[7],
+      a33 = this.values[8];
 
-    const b11=matrix.values[0],b12=matrix.values[1], b13=matrix.values[2];
-    const b21=matrix.values[3],b22=matrix.values[4], b23=matrix.values[5];
-    const b31=matrix.values[6],b32=matrix.values[7], b33=matrix.values[8];
+    const b11 = matrix.values[0],
+      b12 = matrix.values[1],
+      b13 = matrix.values[2];
+    const b21 = matrix.values[3],
+      b22 = matrix.values[4],
+      b23 = matrix.values[5];
+    const b31 = matrix.values[6],
+      b32 = matrix.values[7],
+      b33 = matrix.values[8];
 
     this.values[0] = a11 * b11 + a12 * b21 + a13 * b31;
-    this.values[1] = a11 * b12 + a12
+    this.values[1] = a11 * b12 + a12;
   }
-
 }
